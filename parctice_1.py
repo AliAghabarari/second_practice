757d2347d5f3ab4c2644ed9b498faa93d14fb9fe
AliAghabarari <aliaghabarari213@gmail.com>
2024-03-22 14:56:40 +0430

    Update parctice_1.py

--- parctice_1.py
+++ parctice_1.py
@@ -1,10 +1,27 @@
+import random
+
+
 def decrypt_clue(text):
     mysterious = "ThereareyouIandletlistintgameJupiterlinedefblindasyieldassertbreakclasscontinueexceptfinallyforfromglobalimportnonlocalpassraisereturntrywhilewithandorasassertbreakclasscontinuedefdelelifelseexceptexecfinallyforfromglobalifimportinislambdnonlocalnotorpassprintraisereprreturnsupertrywhilewithyieldTrueFalseNoneasyncawaitmatchcasenonelocaloverrideprivatesealedstaticmethodvolatileabstractenumintfloatdoublebyteboolcharstructvectorlistdictionaryqueuestackinterfacenulltrycatchfinallythrowthrowsimplementsextendspublicprotectedprivatefinalstaticvoidmainStringargsSystemoutprintlnnewMapSetGetAddRemoveClearIsEmptySizeContainsKeyContainsValueKeyValuePairsForEachDoWhileSwitchCaseDefaultbreakcontinueiteratoriterablecollectionsframeworksynchronizedtransientvolatilestrictfpbitwiselogicalshiftcompoundassignmenttypeinferencegenericswildcardstypeerasurereflectionproxydecoratorfactorysingletonprototypebuildercompositeadapterbridgechainofresponsibilitycommandstateobservermementointerpreteriteratorvisitorstrategytemplatemethodflyweightmediatorproxymultithreadingconcurrencyasynchronousprogrammingeventdrivenarchitecturefunctionalprogrammingimmutables...quantumcomputingqubitsentanglementsuperpositionquantumalgorithmsShorsGroverquantumcryptographyquantumteleportationartificialintelligenceAIoptimizationalgorithmsgraphtheorytraversalsearchalgorithmsdepthfirstsearchbreadthfirstsearchAstaralgorithmgeneticalgorithmssimulatedannealingparticlewarmoptimizationantcolonyoptimizationmachinelearningdeeplearningunsupervisedlearningreinforcementlearningneuralnetworksconvolutionalneuralnetworksrecurrentneuralnetworkslongshorttermmemorynetworksgenerativeadversar...blockchaintechnologydistributedledgerconsensusalgorithmsproofOfWorkproofOfStakeByzantineFaultTolerancepeer-to-peerP2PnetworkscryptographyhashfunctionsasymmetricencryptionpublickeyinfrastructurePKIdigitalcertificatesdigital signaturesmartcontractsdecentralizedapplicationsDAppsEthereumplatformSolidityprogramminglanguageWeb3technologyNFTsnon-fungibletokensDeFidecentralizedfinancedecentralizedexchangesDEXsstablecoinscryptocurrencyminingstakingyieldfarmingliquiditypoolsoraclesDAOsdecentralizedautonomousorganizati..."
+    a = []
+    for i in text:
+        if i in mysterious:
+            a.append(i)
+    return a
 
-    if text in mysterious:
-        return text
-
-
+def solve_puzzle(text):
+    puzzles = ['ali', '1233', '0', '', '[]', '{}', "'False'", "'0'", "'None'",
+               'None', '-1', '[1,2,3]',"{'key':'value'}", 'True', "''", "'[]'",
+               "'[1,2,3]'", "'{}'", "'{'a':1}'", "'True'", "'ali'", "'1234'", '1',
+               '0.1', '-0.1', 'True', "''", "'[]'", "'[1,2,3]'", "'{}'", "'{'a':1}'",
+               "'True'", "'ali'", "'1234'", '1', '0.1', '-0.1', 'True', "''", "'[]'",
+               "'[1,2,3]'","'{}'", "'{'a':1}'", "'True'", "'ali'", "'1234'", '1', '0.1']
+    for i in puzzles:
+        if i == '1' or i == 'True':
+            text.append(True)
+        elif i == '0' or i == 'False':
+            text.append(False)
+    return text
 keywords = 'False class from or None continue global pass True def if raise and del import return as elif in try assert else is while async except lambda with await finally nonlocalyield break for not'
 
 x = len(keywords)
@@ -22,8 +39,10 @@ print(keys)
 
 #print(keys)
 is_exist = []
-for i in keys:
-    r = decrypt_clue(i)
-    is_exist.append(r)
-
+is_exist = decrypt_clue(keys)
 print(is_exist)
+
+puzzles = []
+solve_puzzle(puzzles)
+
+print(puzzles)
